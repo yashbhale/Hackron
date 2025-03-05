079b1db3516e4a486cbcd78240048814263bb9ce
yashbhale <yashvivekbhale@gmail.com>
2025-03-05 18:12:33 +0530

    heelo

--- base/app/components/Navbar.js
+++ base/app/components/Navbar.js
@@ -3,7 +3,7 @@ import { FiMapPin, FiSearch, FiUser, FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <nav className="bg-yellow-200 text-black shadow-lg fixed w-full top-0 z-50">
+    <nav className="bg-gray-100 text-black shadow-lg fixed w-full top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Left Section */}
