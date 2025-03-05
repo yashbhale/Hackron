053a03cbd3af31c41edb8bd304b623c64a646727
Vaibhavpokale980 <vaibhavpokale980@gmail.com>
2025-03-05 23:49:01 +0530

    bhala12

--- ml/app.py
+++ ml/app.py
@@ -13,7 +13,7 @@ from folium.plugins import HeatMap
 app = Flask(__name__)
 
 # Load Customer Data (global copy)
-df = pd.read_csv(r'C:\Users\yashv\OneDrive\Documents\Desktop\Hackron\ml\Customer_data.csv')
+df = pd.read_csv(r'C:\Desktop\hackron\Hackron\ml\Customer_data.csv')
 df['order_count'] = df['order_count'].fillna(0) + 1  # Fill NaN with 0, then add 1
 df.dropna(subset=['Delivery_location_latitude', 'Delivery_location_longitude'], inplace=True)
 
@@ -33,7 +33,8 @@ def get_clusters():
             kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
             labels = kmeans.fit_predict(customer_locations, sample_weight=customer_weights)
             silhouette_scores.append(silhouette_score(customer_locations, labels))
-        optimal_k = K_range[np.argmax(silhouette_scores)]
+            optimal_k = K_range[np.argmax(silhouette_scores)]
+            optimal_k = 11
 
 
         # Apply Clustering
